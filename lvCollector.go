f239698d8e6a0b6c00e7aa0790bec945e152f93f
Lukas Fuchs <panzelva@gmail.com>
2020-10-15 22:33:41 +0200

    add volume group name to lv collector

--- lvCollector.go
+++ lvCollector.go
@@ -20,19 +20,19 @@ func newLvmLvCollector() *lvmLvCollector {
 	return &lvmLvCollector{
 		lvDataSizeMetric: prometheus.NewDesc("lvm_lv_size_bytes",
 			"Shows LVM LV data size in Bytes",
-			[]string{"lv_pool_name", "lv_name"}, nil,
+			[]string{"lv_pool_name", "lv_name", "vg_name"}, nil,
 		),
 		lvMetadataSizeMetric: prometheus.NewDesc("lvm_lv_metadata_size_bytes",
 			"Shows LVM LV metadata size in Bytes",
-			[]string{"lv_pool_name", "lv_name"}, nil,
+			[]string{"lv_pool_name", "lv_name", "vg_name"}, nil,
 		),
 		lvDataFilledMetric: prometheus.NewDesc("lvm_lv_filled_percentage",
 			"Shows LVM LV data filled percentage",
-			[]string{"lv_pool_name", "lv_name"}, nil,
+			[]string{"lv_pool_name", "lv_name", "vg_name"}, nil,
 		),
 		lvMetadataFilledMetric: prometheus.NewDesc("lvm_lv_metadata_filled_percentage",
 			"Shows LVM LV metadata filled percentage",
-			[]string{"lv_pool_name", "lv_name"}, nil,
+			[]string{"lv_pool_name", "lv_name", "vg_name"}, nil,
 		),
 	}
 }
@@ -45,7 +45,8 @@ func (collector *lvmLvCollector) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (collector *lvmLvCollector) Collect(ch chan<- prometheus.Metric) {
-	out, err := exec.Command("/sbin/lvs", "--units", "B", "--separator", ",", "-o", "lv_size,lv_metadata_size,data_percent,metadata_percent,pool_lv,lv_name", "--noheadings").Output()
+	// /sbin/lvs --units B --separator , -o lv_size,lv_metadata_size,data_percent,metadata_percent,pool_lv,lv_name,vg_name --noheadings
+	out, err := exec.Command("/sbin/lvs", "--units", "B", "--separator", ",", "-o", "lv_size,lv_metadata_size,data_percent,metadata_percent,pool_lv,lv_name,vg_name", "--noheadings").Output()
 	if err != nil {
 		log.Print(err)
 	}
@@ -60,6 +61,7 @@ func (collector *lvmLvCollector) Collect(ch chan<- prometheus.Metric) {
 		descriptors := []*prometheus.Desc{collector.lvDataSizeMetric, collector.lvMetadataSizeMetric, collector.lvDataFilledMetric, collector.lvMetadataFilledMetric}
 		poolName := values[4]
 		logicalVolumeName := values[5]
+		volumeGroupName := values[6]
 
 		for index, descriptor := range descriptors {
 			value, err := strconv.ParseFloat(strings.Trim(values[index], "B"), 64)
@@ -67,7 +69,7 @@ func (collector *lvmLvCollector) Collect(ch chan<- prometheus.Metric) {
 				continue
 			}
 
-			ch <- prometheus.MustNewConstMetric(descriptor, prometheus.GaugeValue, value, poolName, logicalVolumeName)
+			ch <- prometheus.MustNewConstMetric(descriptor, prometheus.GaugeValue, value, poolName, logicalVolumeName, volumeGroupName)
 		}
 	}
 }
